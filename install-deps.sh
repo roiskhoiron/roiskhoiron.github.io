19fdfbed86c3b5bd8ea3ed6fe0238d34a7e5acad
roishoiron <rois.khoiron@gmail.com>
2026-09-12 23:48:41 +0700

    chore: update bun.lock

---
+++ install-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "/Users/roishoiron/Library/Application Support/Open Design/namespaces/release-stable/data/projects/4d32e448-32c1-4373-a75c-a74d99d38428"
+rm -rf node_modules package-lock.json .package-lock.json
+npm install
